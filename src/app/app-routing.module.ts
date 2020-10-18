d92e845f2580ccd144401340b3467fbcc58fd252
unknown <Hashtag#19>
2020-10-17 20:58:40 -0700

    updated routing URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,28 @@ import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-
+import { RoutesComponent } from './routes/routes.component';
+import { AddRoutesComponent } from './add-routes/add-routes.component';
+import { ShowRoutesComponent } from './show-routes/show-routes.component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [
   {path: '' , component:LoginComponent},
-  {path: 'admin', component: AdminComponent},
+  {path: 'admin', component: AdminComponent,
+  children:[
+    {path:'routes',component:RoutesComponent},
+    {path:'add-routes',component:AddRoutesComponent},
+    {path:'show-routes',component:ShowRoutesComponent}
+  ]},
   {path: 'home', component: HomeComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),CommonModule,BrowserModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
